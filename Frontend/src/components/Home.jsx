d4c53c6edd6ceda6ce7de02b5f0914cbb7633070
mvishalsingh25 <mvishalsingh25@gmail.com>
2025-07-30 16:30:13 +0530

    url

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -43,7 +43,8 @@ function Home() {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/convertFile",
+        // "http://localhost:3000/convertFile",
+        "https://wordtopdf-backend-fet2.onrender.com/convertFile",
         formData,
         { responseType: "blob" }
       );
